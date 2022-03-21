18a09ee5e632daeee9ce872006aa2e6b9dbcaab6
183764766@qq.com <962464Ling>
2022-03-21 09:53:28 +0800

    init

---
+++ app/src/main/java/com/cxj/composedemo/MainComposeActivity.kt
@@ -0,0 +1,134 @@
+package com.cxj.composedemo
+
+import android.content.res.Configuration
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.cxj.composedemo.ui.theme.ComposeDemoTheme
+
+class MainComposeActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeDemoTheme {
+                // A surface container using the 'background' color from the theme
+//                Surface(
+//                    modifier = Modifier.fillMaxSize(),
+//                    color = MaterialTheme.colors.background
+//                ) {
+//
+//                }
+
+                MessageCard(msg = Message("Android","Jetpack Compose"))
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+data class Message(val author: String, val body: String)
+
+
+@Composable
+fun MessageCard(msg: Message) {
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_background),
+            contentDescription = "ic_launcher",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+
+        // We keep track if the message is expanded or not in this
+        // variable
+        var isExpanded by remember { mutableStateOf(false) }
+
+        // surfaceColor will be updated gradually from one color to the other
+        val surfaceColor: Color by animateColorAsState(
+            if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface,
+        )
+
+        Column(modifier = Modifier.clickable {
+            isExpanded = !isExpanded
+        }) {
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colors.secondaryVariant,
+                style = MaterialTheme.typography.subtitle2
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                elevation = 1.dp,
+                // surfaceColor color will be changing gradually from primary to surface
+                color = surfaceColor,
+                // animateContentSize will change the Surface size gradually
+                modifier = Modifier
+                    .animateContentSize()
+                    .padding(1.dp)
+            ) {
+                Text(
+                    text = msg.body,
+                    style = MaterialTheme.typography.body2,
+                    // If the message is expanded, we display all its content
+                    // otherwise we only display the first line
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                    modifier = Modifier.padding(all = 4.dp)
+                )
+            }
+
+        }
+    }
+
+
+}
+
+
+@Composable
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) { message ->
+            MessageCard(msg = message)
+        }
+    }
+}
+
+@Preview(name = "Dark Mode")
+@Preview(
+    showBackground = true,
+    name = "Light Mode",
+    uiMode = Configuration.UI_MODE_NIGHT_YES
+)
+@Composable
+fun DefaultPreview() {
+    ComposeDemoTheme {
+//        MessageCard(msg = Message("Android", "Jetpack Compose"))
+        Conversation(messages = SampleData.conversationSample)
+    }
+}
---
+++ app/src/main/java/com/cxj/composedemo/SampleData.kt
@@ -0,0 +1,20 @@
+package com.cxj.composedemo
+
+object SampleData {
+    val conversationSample = listOf(
+        Message("Colleague","testtesttesttesttest"),
+        Message("Colleague","test2test2test2test2test2test2test2"),
+        Message("Colleague","test3test3test3test3test3test3test3test3test3test3test3test3test3test3test3test3test3test3test3test3test3test3test3test3test3"),
+        Message("Colleague","test4test4test4test4test4"),
+        Message("Colleague","test5test5test5test5test5test5test5test5test5test5test5test5"),
+        Message("Colleague","test6test6"),
+        Message("Colleague","test7test7test7test7"),
+        Message("Colleague","test8test8test8test8test8test8test8test8test8test8test8test8test8test8test8test8test8test8test8test8test8test8test8test8test8test8test8test8test8test8test8test8test8test8"),
+        Message("Colleague","test9"),
+        Message("Colleague","test10"),
+        Message("Colleague","test11"),
+        Message("Colleague","test12"),
+        Message("Colleague","test13"),
+        Message("Colleague","test14"),
+    )
+}
